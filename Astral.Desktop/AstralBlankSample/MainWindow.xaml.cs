d76bec8d391a000f242b2fd9b2ebc0e4faefa22b
David Ledo <davidledo89@gmail.com>
2018-05-04 10:44:13 -0600

    Watch Stylus Blank App

---
+++ Astral.Desktop/AstralBlankSample/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Astral;
+using Astral.Device;
+
+namespace AstralBlankSample
+{
+    /*
+     * Couple notes -
+     * Run this system first (this will be the server), then run the Astral Mobile App
+     * Update the Astral Mobile activity as you see fit and don't forget to update the IP Address
+     */
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Instance Variables
+
+        private NetworkManager networkManager = NetworkManager.Instance;
+        private DeviceModel device;
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Start the networking
+            this.networkManager.Start();
+            this.networkManager.DeviceAdded += OnDeviceConnected;
+        }
+
+        #endregion
+
+        #region Connection Initialization
+
+        private void OnDeviceConnected(object sender, DeviceConnectedEventArgs e)
+        {
+            device = new DeviceModel(e.Device, e.Session);
+
+            // Note: all networking events are running in a separate thread
+            // you have to use this dispatcher invoke to do anything with the UI
+            // otherwise you'll get a weird exception you can't make sense of
+            Dispatcher.Invoke(new Action(delegate
+            {
+                
+            }));
+            
+            InitializeDeviceEvents();
+        }
+
+        private void InitializeDeviceEvents()
+        {
+            device.Display.TouchDown += OnTouchDown;
+            device.Accelerometer.AccelerationChanged += AccelerometerUpdated;
+            device.Microphone.MicrophoneUpdated += OnMicrophoneUpdated;
+
+            // newer version of the accelerometer
+            device.AccelerationChanged += OnAccelerationChanged;
+        }
+
+        #endregion
+
+        private void AccelerometerUpdated(object sender, AstralAccelerometerEventArgs e)
+        {
+            Console.WriteLine(e.AccelerationData.X + " :: " + e.AccelerationData.Y + " :: " + e.AccelerationData.Z);
+        }
+
+        private void OnMicrophoneUpdated(object sender, AstralMicrophoneEventArgs e)
+        {
+            Console.WriteLine(e.MicrophoneData.Amplitude);
+            // full array of values
+            Console.WriteLine(e.MicrophoneData.Data);
+        }
+
+        private void OnTouchDown(object sender, AstralTouchEventArgs e)
+        {
+            Console.WriteLine(e.TouchPoint.X + " :: " + e.TouchPoint.Y);
+        }
+
+        private void OnAccelerationChanged(object sender, AccelerationDeviceModelEventArgs e)
+        {
+            // this event can get linear acceleration, acceleration and gravity values
+            Console.WriteLine(e.LinearX + " :: " + e.LinearY + " :: " + e.LinearZ);
+            Console.WriteLine(e.LinearMagnitude);
+        }
+
+    }
+}
---
+++ Astral.Desktop/AstralBlankSample/Model/DeviceModel.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Astral;
+using Astral.Device;
+using Orientation = Astral.Device.Orientation;
+
+namespace AstralBlankSample
+{
+        #region Enum MobileEventType
+
+        public enum MobileEventType
+        {
+            None = -1,
+            TouchDown = 0,
+            TouchUp = 1,
+            TouchMove = 2,
+            AccelerationChanged = 3,
+            AmbientLightChanged = 5,
+            CompassChanged = 6,
+            MagnetometerChanged = 7,
+            MagnetometerMagnitudeChanged = 8,
+            MagnetometerAngleChanged = 9,
+            GyroscopeChanged = 10,
+            OrientationChanged = 11,
+            AmplitudeChanged = 12,
+            FrequencyChanged = 13
+        }
+
+        #endregion
+
+        #region AccelerationDeviceModelEventArgs
+
+        public class AccelerationDeviceModelEventArgs : EventArgs
+        {
+            private double accelerationX, accelerationY, accelerationZ;
+            private double linearX, linearY, linearZ;
+            private double gravityX, gravityY, gravityZ;
+
+            #region Properties
+
+            public double Magnitude
+            {
+                get
+                {
+                    return Utils.Magnitude(this.accelerationX, this.accelerationY, this.accelerationZ);
+                }
+            }
+
+            public double LinearMagnitude
+            {
+                get
+                {
+                    return Utils.Magnitude(this.linearX, this.linearY, this.linearZ);
+                }
+            }
+
+            public double GravityMagnitude
+            {
+                get
+                {
+                    return Utils.Magnitude(this.gravityX, this.gravityY, this.gravityZ);
+                }
+            }
+
+            public double AccelerationX
+            {
+                get
+                {
+                    return this.accelerationX;
+                }
+                internal set
+                {
+                    this.accelerationX = value;
+                }
+            }
+
+            public double AccelerationY
+            {
+                get
+                {
+                    return this.accelerationY;
+                }
+                internal set
+                {
+                    this.accelerationY = value;
+                }
+            }
+
+            public double AccelerationZ
+            {
+                get
+                {
+                    return this.accelerationZ;
+                }
+                internal set
+                {
+                    this.accelerationZ = value;
+                }
+            }
+
+            public double GravityX
+            {
+                get
+                {
+                    return this.gravityX;
+                }
+            }
+
+            public double GravityY
+            {
+                get
+                {
+                    return this.gravityY;
+                }
+            }
+
+            public double GravityZ
+            {
+                get
+                {
+                    return this.gravityZ;
+                }
+            }
+
+            public double LinearX
+            {
+                get
+                {
+                    return this.linearX;
+                }
+            }
+
+            public double LinearY
+            {
+                get
+                {
+                    return this.linearY;
+                }
+            }
+
+            public double LinearZ
+            {
+                get
+                {
+                    return this.linearZ;
+                }
+            }
+
+            #endregion
+
+            private void SetGravityAndLinear()
+            {
+                // get the gravity
+                float alpha = 0.8f;
+
+                // Isolate the force of gravity with the low-pass filter.
+                this.gravityX = alpha * gravityX + (1 - alpha) * accelerationX;
+                this.gravityY = alpha * gravityY + (1 - alpha) * accelerationY;
+                this.gravityZ = alpha * gravityZ + (1 - alpha) * accelerationZ;
+
+                // now that we have gravity get the linear
+                // using a high pass filter
+                linearX = accelerationX - gravityX;
+                linearY = accelerationY - gravityY;
+                linearZ = accelerationZ - gravityZ;
+            }
+
+            public AccelerationDeviceModelEventArgs(double x, double y, double z)
+            {
+                this.accelerationX = x;
+                this.accelerationY = y;
+                this.accelerationZ = z;
+                SetGravityAndLinear();
+            }
+        }
+
+        #endregion
+
+        #region Class DeviceModel
+
+        public class DeviceModel
+        {
+            #region Instance Variables
+            private AstralDevice device;
+            private Display display;
+            private Accelerometer accelerometer;
+            private Compass compass;
+            private Magnetometer magnetometer;
+            private AmbientLight ambientLight;
+            private Orientation orientation;
+            private Gyroscope gyroscope;
+            private Microphone microphone;
+            private AstralSession session;
+
+            private MovingAverageFilter accFilterX = new MovingAverageFilter();
+            private MovingAverageFilter accFilterY = new MovingAverageFilter();
+            private MovingAverageFilter accFilterZ = new MovingAverageFilter();
+
+            #endregion
+
+            #region Properties
+            public AstralDevice Device { get => device; set => device = value; }
+            public Display Display { get => display; set => display = value; }
+            public Accelerometer Accelerometer { get => accelerometer; set => accelerometer = value; }
+            public Compass Compass { get => compass; set => compass = value; }
+            public Magnetometer Magnetometer { get => magnetometer; set => magnetometer = value; }
+            public AmbientLight AmbientLight { get => ambientLight; set => ambientLight = value; }
+            public Orientation Orientation { get => orientation; set => orientation = value; }
+            public Gyroscope Gyroscope { get => gyroscope; set => gyroscope = value; }
+            public Microphone Microphone { get => microphone; set => microphone = value; }
+
+            public string Name { get => this.device.Name; }
+            public string Class { get => this.device.Class; }
+
+            public AstralSession Session { get => session; }
+
+            public Rect InputRegion
+            {
+                get
+                {
+                    return this.session.InputRegion;
+                }
+                set
+                {
+                    this.session.SetInputRegion(value);
+                }
+            }
+
+            public Rect CaptureRegion
+            {
+                get
+                {
+                    return this.session.CaptureRegion;
+                }
+                set
+                {
+                    this.session.SetCaptureRegionLocation(value.TopLeft);
+                    this.session.SetCaptureRegionWidth((int)value.Width);
+                }
+            }
+
+            #endregion
+
+            public event EventHandler<AccelerationDeviceModelEventArgs> AccelerationChanged;
+
+            private void RaiseAccelerationChanged(AccelerationDeviceModelEventArgs e)
+            {
+                AccelerationChanged?.Invoke(this, e);
+            }
+
+            #region Constructor
+
+            public DeviceModel(AstralDevice device, AstralSession session)
+            {
+                this.Device = device;
+                this.session = session;
+
+                if (device.HasDisplay) this.Display = device[ModuleType.Display] as Display;
+                if (device.HasAccelerometer) this.accelerometer = device[ModuleType.Accelerometer] as Accelerometer;
+                if (device.HasAmbientLight) this.ambientLight = device[ModuleType.AmbientLight] as AmbientLight;
+                if (device.HasCompass) this.compass = device[ModuleType.Compass] as Compass;
+                if (device.HasMagnetometer) this.magnetometer = device[ModuleType.Magnetometer] as Magnetometer;
+                if (device.HasGyroscope) this.gyroscope = device[ModuleType.Gyroscope] as Gyroscope;
+                if (device.HasOrientation) this.orientation = device[ModuleType.Orientation] as Orientation;
+                if (device.HasMicrophone) this.microphone = device[ModuleType.Microphone] as Microphone;
+
+                this.accelerometer.AccelerationChanged += OnAccelerationChanged;
+
+            }
+
+            private void OnAccelerationChanged(object sender, AstralAccelerometerEventArgs e)
+            {
+                this.accFilterX.ComputeAverage(e.AccelerationData.X);
+                this.accFilterY.ComputeAverage(e.AccelerationData.Y);
+                this.accFilterZ.ComputeAverage(e.AccelerationData.Z);
+
+                RaiseAccelerationChanged(new AccelerationDeviceModelEventArgs(this.accFilterX.Average, this.accFilterY.Average, this.accFilterZ.Average));
+            }
+
+            #endregion
+
+            public void ShowCaptureWindow()
+            {
+                this.session.ShowCaptureSelectionWindow();
+            }
+
+            public void ShowInputWindow()
+            {
+                this.session.ShowInputSelectionWindow();
+            }
+
+        }
+
+        #endregion
+    
+}
